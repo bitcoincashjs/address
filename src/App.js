51df3346f3638280608db9fa60bf2ac1ce6e9a70
Emilio Almansi <ealmansi@gmail.com>
2018-01-17 22:20:58 -0300

    Now using BchAddr.js.

--- src/App.js
+++ src/App.js
@@ -1,170 +1,16 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'toastr/build/toastr.css'
-import toastr from 'toastr/build/toastr.min.js'
+import { Network, Type, toLegacyAddress, toBitpayAddress, toCashAddress, detectAddressNetwork, detectAddressType } from 'bchaddrjs';
+import $ from 'jquery';
+import copy from 'copy-to-clipboard';
 import FaCopy from 'react-icons/lib/fa/copy';
 import FaQuestionCircle from 'react-icons/lib/fa/question-circle';
-import copy from 'copy-to-clipboard';
-import bch from 'bitcoincashjs';
-import './App.css';
-
-const Address = bch.Address;
-const LegacyFormat = Address.LegacyFormat;
-const CashAddrFormat = Address.CashAddrFormat;
-const BitpayFormat = Address.BitpayFormat;
+import React, { Component } from 'react';
+import toastr from 'toastr/build/toastr.min.js'
 
 class App extends Component {
-
-  constructor(props) {
-    super(props)
-    this.state = {
-      input: '',
-      prefixed: true,
-      hasOutput: false,
-      network: '',
-      type: '',
-      legacy: '',
-      cashaddr: '',
-      bitpay: '',
-    }
-  }
-
-  onInputChange(event){
-    const input = event.target.value.trim();
-    const option = this.decodeAddress(input);
-    if (option.empty) {
-      this.setState({
-        input: input,
-        hasOutput: false,
-        network: '',
-        type: '',
-        legacy: '',
-        cashaddr: '',
-        bitpay: '',
-      });
-    }
-    else {
-      const address = option.value;
-      const network = address.network.name;
-      const type = address.type;
-      const cashaddr = address.toString(CashAddrFormat);
-      const prefixed = this.state.prefixed
-          || (input === cashaddr && input.indexOf(':') !== -1);
-      const legacy = address.toString(LegacyFormat);
-      const cashaddrDisplay = prefixed ?
-          cashaddr :
-          cashaddr.split(':')[1];
-      const bitpay = address.toString(BitpayFormat);
-      this.setState({
-        input: input,
-        prefixed: prefixed,
-        network: network,
-        type: type,
-        hasOutput: true,
-        legacy: legacy,
-        cashaddr: cashaddrDisplay,
-        bitpay: bitpay,
-      });
-    }
-  }
-
-  decodeAddress(input) {
-    let result = this.optional(Address.fromString, input, null, null, LegacyFormat);
-    if (result.empty) {
-      result = this.optional(Address.fromString, input, null, null, BitpayFormat);
-    }
-    if (result.empty) {
-      for (const network of ['livenet', 'testnet']) {
-        for (const type of [Address.PayToPublicKeyHash, Address.PayToScriptHash]) {
-          const prefix = network === 'livenet' ? 'bitcoincash' : 'bchtest';
-          result = this.optional(Address.fromString, input, network, type, CashAddrFormat);
-          if (result.empty) {
-            result = this.optional(Address.fromString, `${prefix}:${input}`, network, type, CashAddrFormat);
-          }
-          if (!result.empty) {
-            break;
-          }
-        }
-        if (!result.empty) {
-          break;
-        }
-      }
-    }
-    return result;
-  }
-
-  optional(fn, ...args) {
-    try {
-      return {
-        empty: false,
-        value: fn(...args)
-      };
-    }
-    catch (err) {
-      return {
-        empty: true,
-        value: null
-      }
-    }
-  }
-
-  onNetworkChange(event) {
-    this.setState({
-      network: event.target.value,
-    });
-  }
-
-  onPrefixedChange(event) {
-    const prefixed = $(event.target).is(':checked');
-    if (!this.state.hasOutput) {
-      this.setState({
-        prefixed: prefixed,
-      });
-    }
-    else {
-      if (prefixed && this.state.cashaddr.indexOf(':') === -1) {
-        const prefix = this.state.network === 'livenet' ? 'bitcoincash' : 'bchtest';
-        const cashaddr = `${prefix}:${this.state.cashaddr}`;
-        this.setState({
-          prefixed: prefixed,
-          cashaddr: cashaddr,
-        });
-      }
-      else if (!prefixed && this.state.cashaddr.indexOf(':') !== -1) {
-        this.setState({
-          prefixed: prefixed,
-          cashaddr: this.state.cashaddr.split(':')[1],
-        });
-      }
-    }
-  }
-
-  copyLegacy() {
-    const success = copy(this.state.legacy);
-    this.afterCopy(success);
-  }
-
-  copyCashAddr() {
-    const success = copy(this.state.cashaddr);
-    this.afterCopy(success);
-  }
-
-  copyBitpay() {
-    const success = copy(this.state.bitpay);
-    this.afterCopy(success);
-  }
-
-  afterCopy(success) {
-    if (success) {
-      toastr.success('Address copied to clipboard.', '', { timeOut: 1000 });
-    }
-    else {
-      toastr.warning('Could not copy address to clipboard.', '', { timeOut: 1000 });
-    }
-  }
-
   render() {
     return (
       <div className="App">
@@ -180,49 +26,7 @@ class App extends Component {
                     </div>
                   </div>
                 </div>
-                <div className="Translator">
-                  <div className="card">
-                    <div className="card-body">
-                      <div className="address-input">
-                        <label htmlFor="address">Enter the address you wish to translate:</label>
-                        <div className="input-group">
-                          <div className="input-group-prepend">
-                            <span className="input-group-text">Address</span>
-                          </div>
-                          <input value={this.state.input} onChange={this.onInputChange.bind(this)} type="text" className="form-control" id="address" />
-                        </div>
-                      </div>
-                      <form className="form-inline">
-                        <div className="form-group mx-sm-1 mb-1">
-                          <div className="form-check form-check-inline">
-                            <input checked={this.state.prefixed} onChange={this.onPrefixedChange.bind(this)} className="form-check-input" type="checkbox" id="prefix-input" value="option1" />
-                            <label className="form-check-label" htmlFor="prefix-input">Display with prefix</label>
-                          </div>
-                        </div>
-                      </form>
-                      <span className="invalid-address"><em>{ this.state.input && !this.state.hasOutput ? 'This is not a valid Bitcoin Cash address.' : '' }</em></span>
-                      {
-                        this.state.hasOutput ?
-                        (
-                          <div className="output">
-                            <hr />
-                            <div className="legacy-output"><strong>Legacy format:</strong> <span onClick={this.copyLegacy.bind(this)}> { this.state.legacy }</span>  <button type="button" className="btn btn-light btn-sm" onClick={this.copyLegacy.bind(this)}><FaCopy /></button></div>
-                            <div className="cashaddr-output"><strong>Cash Address format:</strong> <span onClick={this.copyCashAddr.bind(this)}> { this.state.cashaddr } </span> <button type="button" className="btn btn-light btn-sm" onClick={this.copyCashAddr.bind(this)}><FaCopy /></button></div>
-                            <div className="bitpay-output"><strong>Bitpay format:</strong> <span onClick={this.copyBitpay.bind(this)}> { this.state.bitpay } </span>  <button type="button" className="btn btn-light btn-sm" onClick={this.copyBitpay.bind(this)}><FaCopy /></button></div>
-                            <em>
-                              This is a {
-                                this.state.network === 'livenet' ? 'mainnet' : 'testnet'
-                              } address, of type {
-                                this.state.type === 'pubkeyhash' ? 'Pay-to-Public-Key-Hash' : 'Pay-to-Script-Hash'
-                              }.
-                            </em>
-                          </div>
-                        ) :
-                        ''
-                      }
-                    </div>
-                  </div>
-                </div>
+                <TranslatorContainer copyText={this.copyText}/>
               </div>
             </div>
           </div>
@@ -254,6 +58,180 @@ class App extends Component {
       </div>
     );
   }
+
+  copyText(text) {
+    if (copy(text)) {
+      toastr.success('Address copied to clipboard.', '', { timeOut: 1000 });
+    }
+    else {
+      toastr.warning('Could not copy address to clipboard.', '', { timeOut: 1000 });
+    }
+  }
+}
+
+class TranslatorContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      addressInputValue: '',
+      prefixCheckboxValue: true,
+    }
+  }
+
+  onAddressInputChange(event) {
+    this.setState({
+      addressInputValue: event.target.value.trim(),
+    });
+  }
+
+  onPrefixCheckboxChange(event) {
+    this.setState({
+      prefixCheckboxValue: $(event.target).is(':checked'),
+    });
+  }
+
+  render() {
+    let legacyAddress;
+    let bitpayAddress;
+    let cashAddress;
+    let network;
+    let type;
+    let isValidAddress;
+    try {
+      isValidAddress = true;
+      legacyAddress = toLegacyAddress(this.state.addressInputValue);
+      bitpayAddress = toBitpayAddress(this.state.addressInputValue);
+      cashAddress = toCashAddress(this.state.addressInputValue);
+      network = detectAddressNetwork(this.state.addressInputValue);
+      type = detectAddressType(this.state.addressInputValue);
+      if (cashAddress.indexOf(':') === -1 && this.state.prefixCheckboxValue) {
+        cashAddress = networkToCashPrefix(network) + ':' + cashAddress;
+      }
+      else if (cashAddress.indexOf(':') !== -1 && !this.state.prefixCheckboxValue) {
+        cashAddress = cashAddress.split(':')[1];
+      }
+    }
+    catch (error) {
+      isValidAddress = false;
+    }
+    const copyText = this.props.copyText;
+    return (
+      <Translator
+        addressInputValue={this.state.addressInputValue}
+        onAddressInputChange={this.onAddressInputChange.bind(this)}
+        prefixCheckboxValue={this.state.prefixCheckboxValue}
+        onPrefixCheckboxChange={this.onPrefixCheckboxChange.bind(this)}
+        isValidAddress={isValidAddress}
+        legacyAddress={legacyAddress}
+        bitpayAddress={bitpayAddress}
+        cashAddress={cashAddress}
+        network={network}
+        type={type}
+        copyText={copyText}
+      />
+    );
+  }
+}
+
+function networkToCashPrefix(network) {
+  switch (network) {
+    case Network.Mainnet:
+      return 'bitcoincash';
+    default:
+      return 'bchtest';
+  }
+}
+
+function Translator(props) {
+  const {
+    addressInputValue,
+    onAddressInputChange,
+    prefixCheckboxValue,
+    onPrefixCheckboxChange,
+    isValidAddress,
+    legacyAddress,
+    bitpayAddress,
+    cashAddress,
+    network,
+    type,
+    copyText,
+  } = props;
+  let error, output;
+  if (addressInputValue.length > 0) {
+    if (isValidAddress) {
+      output = (
+        <TranslatorOutput
+          legacyAddress={legacyAddress}
+          bitpayAddress={bitpayAddress}
+          cashAddress={cashAddress}
+          network={network}
+          type={type}
+          copyText={copyText}
+        />
+      );
+    }
+    else {
+      error = (
+        <span className="invalid-address">
+          <em>This is not a valid Bitcoin Cash address.</em>
+        </span>
+      );
+    }
+  }
+  return (
+    <div className="Translator">
+      <div className="card">
+        <div className="card-body">
+          <div className="address-input">
+            <label htmlFor="address">Enter the address you wish to translate:</label>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text">Address</span>
+              </div>
+              <input value={addressInputValue} onChange={onAddressInputChange} type="text" className="form-control" id="address" />
+            </div>
+          </div>
+          <form className="form-inline">
+            <div className="form-group mx-sm-1 mb-1">
+              <div className="form-check form-check-inline">
+                <input checked={prefixCheckboxValue} onChange={onPrefixCheckboxChange} className="form-check-input" type="checkbox" id="prefix-input" value="option1" />
+                <label className="form-check-label" htmlFor="prefix-input">Display with prefix</label>
+              </div>
+            </div>
+          </form>
+          { output }
+          { error }
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function TranslatorOutput(props) {
+  const {
+    legacyAddress,
+    bitpayAddress,
+    cashAddress,
+    network,
+    type,
+    copyText,
+  } = props;
+  const networkName = network === Network.Mainnet ? 'mainnet' : 'testnet';
+  const typeName = type === Type.P2PKH ? 'Pay-to-Public-Key-Hash' : 'Pay-to-Script-Hash';
+  const copyLegacy = () => copyText(legacyAddress);
+  const copyCashAddr = () => copyText(cashAddress);
+  const copyBitpay = () => copyText(bitpayAddress);
+  return (
+    <div className="output">
+      <hr />
+      <div className="legacy-output"><strong>Legacy format:</strong> <span onClick={copyLegacy}> {legacyAddress}</span>  <button type="button" className="btn btn-light btn-sm" onClick={copyLegacy}><FaCopy /></button></div>
+      <div className="cashaddr-output"><strong>Cash Address format:</strong> <span onClick={copyCashAddr}> {cashAddress} </span> <button type="button" className="btn btn-light btn-sm" onClick={copyCashAddr}><FaCopy /></button></div>
+      <div className="bitpay-output"><strong>Bitpay format:</strong> <span onClick={copyBitpay}> {bitpayAddress} </span>  <button type="button" className="btn btn-light btn-sm" onClick={copyBitpay}><FaCopy /></button></div>
+      <em>
+        This is a {networkName} address, of type {typeName}.
+      </em>
+    </div>
+  );
 }
 
 export default App;
